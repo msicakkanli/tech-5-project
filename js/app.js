414215d9deefd448fb57c56c98f4633db42b41a6
msicakkanli <msicakkanli@protein.tech>
2018-04-04 21:18:53 +0300

    add modal, configure modal html

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+let queryResult =[];
 
 $(document).ready(function () {
     $.getJSON('https://randomuser.me/api/?results=12', function (response) {
@@ -6,20 +7,37 @@ $(document).ready(function () {
        if (i === 0 || i === 3 || i === 6 || i === 9 ) {
            bodyHTML += '<container class="list">'
        } 
-       bodyHTML += '<div class="employee" id="'+i+'">'
-       bodyHTML += '<img class="image" src="'+ response.results[i].picture.medium + '" alt="Smiley face">'
-       bodyHTML += '<ul class="empList">'
-       bodyHTML += '<li>' + response.results[i].name.first +' '+ response.results[i].name.last + '</li>'
-       bodyHTML += '<li>' + response.results[i].email + '</li>'
-       bodyHTML += '<li>' + response.results[i].location.city + '</li>'
+       bodyHTML += '<a href="#openModal">'
+       bodyHTML += '<div  class="employee" id="'+i+'">'
+       bodyHTML += '<img class="image" id="'+i+'" src="'+ response.results[i].picture.medium + '" alt="Smiley face">'
+       bodyHTML += '<ul class="empList" id="'+i+'" >'
+       bodyHTML += '<li id="'+i+'" >' + response.results[i].name.first +' '+ response.results[i].name.last + '</li>'
+       bodyHTML += '<li id="'+i+'" >' + response.results[i].email + '</li>'
+       bodyHTML += '<li id="'+i+'" >' + response.results[i].location.city + '</li>'
        bodyHTML += '</ul>'
        bodyHTML += '</div>'
+       bodyHTML += '</a>'
        if (i === 2 || i === 5 || i === 8 || i === 11 ) {
         bodyHTML += '</container>'
+    
     } 
    }
    $('#master').html(bodyHTML);
+   queryResult.push(response);
+   console.log(queryResult);
+   
     })
 })
 
+$(document).on('click','#master', function (event) {
+    let clickId = event.target.id;
+    let clickNum = parseInt(clickId);
+    
+    let modalHTML = ""; 
+    modalHTML += '<a href="#close" title="Close" class="close">X</a>'
+    modalHTML += '<img class="image" id="'+i+'" src="'+ queryResult[0].results[clickNum].picture.medium + '" alt="Smiley face">'
+    modalHTML += '<p>'+ queryResult[0].results[clickNum].name.first +' '+queryResult[0].results[clickNum].name.last +'</p>'
+    modalHTML += '<p>'+ queryResult[0].results[clickNum].location.city +'</p>'
+    $('#detail').html(modalHTML);
+})
 
